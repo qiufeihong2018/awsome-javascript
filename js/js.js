bb229748168947d32e1c4b1270061eaec2742655
qiufeihong <qiufeihong2018@126.com>
2019-03-05 13:49:06 +0800

    js-new

---
+++ js/js.js
@@ -0,0 +1,66 @@
+// function test(name) {
+//     this.name = name
+//     console.log(this)
+// }
+//
+// test.prototype.sayName = function () {
+//     console.log(this.name)
+// }
+// const t = new test('qfh')
+// t.name
+// t.sayName()
+// //new出来的实例,可以访问构造函数的属性.
+// //new出来的实例,可以访问构造函数原型上的方法,通过new,实例与构造函数通过原型链连接了起来.
+// function Test(name) {
+//     this.name = name
+//     // return 'dfasdfasd'
+//     return 231231 || 'dfasdfasdf'
+// }
+//
+// const t = new Test('qfh')
+// console.log(t.name)
+
+// 如果构造函数返回原始值,那么这个构造函数毫无意义
+
+
+// function Test(name) {
+//     this.name = name
+//     return {ddd: 23123}
+// }
+//
+// const t = new Test('qfh')
+// console.log(t.name)
+// 如果构造函数返回对象,返回值会正常使用
+
+
+// new:
+// - new操作符会返回一个对象
+// - 对象可以访问到挂载在this上的任意属性
+// - 对象可以访问到构造函数原型上的属性,所以需要将对象与构造函数链接起来
+// - 返回原始值无效,返回对象有效
+
+function create(Con, ...args) {
+    let obj = {}
+    Object.setPrototypeOf(obj, Con.prototype)
+    let result = Con.apply(obj, args)
+    return result instanceof Object ? result : obj
+
+}
+
+// - Con构造函数,args被构造函数所使用的参数
+// - 创建空对象obj
+// - 等同于 obj._proto_=Con.prototype
+// - 构造函数绑定对象和其余参数
+// - 判断构造函数返回的是否是对象?返回对象:否则就是对象本身
+
+function Test(name) {
+    this.name = name
+    // return '3123123'
+    return {
+        aa: 12312,
+        ddd:234234
+    }
+}
+
+const t = create(Test, 'qfh')
+console.log(t.ddd)
